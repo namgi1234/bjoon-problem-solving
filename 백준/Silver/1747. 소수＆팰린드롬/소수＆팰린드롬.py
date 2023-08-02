eea8c2f6a91443c588a8b4cc2ccafe8c52349a2a
Namki Kwon <75561206+namgi1234@users.noreply.github.com>
2023-08-03 00:29:04 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 3384 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -0,0 +1,21 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def is_palindrome(num):
+    return str(num) == str(num)[::-1]
+
+def find_smallest_palindrome_prime(N):
+    num = N
+    while True:
+        if is_prime(num) and is_palindrome(num):
+            return num
+        num += 1
+
+N = int(input())
+result = find_smallest_palindrome_prime(N)
+print(result)
