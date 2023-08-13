67d8080b3a1785784a69aa6458d601919bda0ad1
Namki Kwon <75561206+namgi1234@users.noreply.github.com>
2023-08-13 13:51:01 +0900

    [Bronze V] Title: Pyramids, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5341. Pyramids/Pyramids.py
+++ 백준/Bronze/5341. Pyramids/Pyramids.py
@@ -1,10 +1,7 @@
-def Pyramids(number):
-    return number * (number + 1) // 2
-
 while True:
     number = int(input())
     
     if number == 0:
         break
         
-    print(Pyramids(number))
+    print(number * (number + 1) // 2)
