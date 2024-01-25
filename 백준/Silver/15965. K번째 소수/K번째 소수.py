64f35f55bf2a824804414bd44b364c27b0b8f981
Namki Kwon <75561206+namgi1234@users.noreply.github.com>
2024-01-25 22:19:40 +0900

    [Silver II] Title: K번째 소수, Time: 1316 ms, Memory: 31120 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/15965. K번째 소수/K번째 소수.py
@@ -0,0 +1,21 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def nth_prime(n):
+    count = 0
+    num = 2
+    while True:
+        if is_prime(num):
+            count += 1
+            if count == n:
+                return num
+        num += 1
+
+n = int(input())
+
+print(nth_prime(n))
