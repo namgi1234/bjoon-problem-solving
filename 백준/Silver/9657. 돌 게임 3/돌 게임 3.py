0273fa1f6b0547fa3f64fa39f659bb8bff1fd458
Namki Kwon <75561206+namgi1234@users.noreply.github.com>
2024-09-15 21:52:05 +0900

    [Silver III] Title: 돌 게임 3, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+dp = [0,1,0,1,1]
+
+if n > 4:
+    for i in range(5,n+1):
+        if dp[i-1] == 1 and dp[i-3] == 1 and dp[i-4] == 1:
+            dp.append(0)
+        else :
+            dp.append(1)
+            
+if dp[n] == 1:
+    print("SK")
+else:
+    print("CY")
