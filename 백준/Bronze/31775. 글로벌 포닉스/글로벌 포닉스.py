e7a3f34cbccce7e9d39b6129e647eb2290b99df1
Namki Kwon <75561206+namgi1234@users.noreply.github.com>
2024-09-16 15:31:18 +0900

    [Bronze IV] Title: 글로벌 포닉스, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/31775. 글로벌 포닉스/글로벌 포닉스.py
@@ -0,0 +1,16 @@
+a = input()
+b = input()
+c = input()
+
+arr =[]
+
+arr.append(a[0])
+arr.append(b[0])
+arr.append(c[0])
+
+arr.sort()
+
+if arr == ['k','l','p']:
+    print("GLOBAL")
+else:
+    print("PONIX")
