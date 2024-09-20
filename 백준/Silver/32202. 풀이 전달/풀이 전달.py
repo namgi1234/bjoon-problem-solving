40101cc8d90d2816b695fa01210f16e090c7ca28
Namki Kwon <75561206+namgi1234@users.noreply.github.com>
2024-09-20 22:12:12 +0900

    [Silver I] Title: 풀이 전달, Time: 236 ms, Memory: 70676 KB -BaekjoonHub

---
+++ 백준/Silver/32202. 풀이 전달/풀이 전달.py
@@ -0,0 +1,20 @@
+def count_ways(N):
+    MOD = 10**9 + 7
+    
+    if N == 0:
+        return 1
+    elif N == 1:
+        return 3
+    
+    dp = [0] * (N + 1)
+    dp[0] = 1
+    dp[1] = 3
+    
+    for i in range(2, N + 1):
+        dp[i] = (2 * dp[i - 1] + 2 * dp[i - 2]) % MOD
+    
+    return dp[N]
+
+# Input reading
+N = int(input())
+print(count_ways(N))
